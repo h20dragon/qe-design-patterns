689bff667e17a9e55a5fad7f02acac6983fd592c
h20dragon <h20dragon@outlook.com>
2019-04-01 17:24:58 -0700

    Updates for 2019.

--- Strategy/strategy_drv.rb
+++ Strategy/strategy_drv.rb
@@ -4,17 +4,49 @@ require 'StrategyAdd'
 require 'StrategyMultiply'
 
 
+def getInt()
+    puts "Enter number: "
+    return gets().strip().to_i
+end
 
-n1=5
-n2=7
 
-add = StrategyAdd.new()
-strategy = StrategyContext.new(add)
-result_add = strategy.executeStrategy(n1,n2)
+def getOperand()
+    puts "ENTER OPERATION (+, *) : "
+    return gets().strip().to_s
+end
 
-multiply = StrategyMultiply.new()
-strategy = StrategyContext.new(multiply)
-result_multiply = strategy.executeStrategy(n1, n2)
 
-puts "Results - Add is " + result_add.to_s
-puts "Results - Multiply : " + result_multiply.to_s
+##
+# MAIN
+##
+
+# Initialize our strategy to use.
+strategy=nil
+
+n1=getInt()
+n2=getInt()
+operand=getOperand()
+
+puts "X: #{n1}"
+puts "Y: #{n2}"
+puts "Operand: #{operand}"
+
+
+# Select the appropriate strategy at run-time!
+if operand=='+' then
+    strategy_to_use = StrategyAdd.new()
+elsif operand=='*' then
+    strategy_to_use = StrategyMultiply.new()
+else
+    puts "Operand is not supported. Abort"
+    exit(1)
+end
+
+# Set the strategy
+strategy = StrategyContext.new(strategy_to_use)
+
+# Execute with the strategy
+result = strategy.executeStrategy(n1, n2)
+
+puts "Result: #{result}"
+
