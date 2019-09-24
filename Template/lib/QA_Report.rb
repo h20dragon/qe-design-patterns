c33daad6f7e9f4cf7572c6140698640a5d84a258
h20dragon <h20dragon@outlook.com>
2019-09-23 21:08:23 -0400

    Add sample report.

--- Template/lib/QA_Report.rb
+++ Template/lib/QA_Report.rb
@@ -4,8 +4,14 @@ class QA_Report < Report
   
   
   def output_details
-    puts "QA_REPORT DETAILS Here"
+     puts '-' * 72
+     puts "QA_REPORT"
+     puts "Passed: 100"
+     puts "Falied:   1"
+     puts "Skipped:  1\n"
+     puts "Result: FAIL
   end
 
   
 end
+
